df0c45ebe429f4753713a332c8784195488301d8
Steven <xhrsteven@gmail.com>
2018-07-30 21:28:26 -0500

    push to heroku

--- config/key.js
+++ config/key.js
@@ -1,2 +1,2 @@
 module.exports =
-  "mongodb://heroku_8hbcfk7r:heroku_8hbcfk7r@ds159641.mlab.com:59641/heroku_8hbcfk7r";
+  "mongodb://heroku_w7125zmlr:heroku_w7125zml@ds159641.mlab.com:59641/heroku_8hbcfk7r";
